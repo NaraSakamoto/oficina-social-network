bb36509b777256ed93c8ae349879010591c94df8
NaraSakamoto <nara.sakamoto@gmail.com>
2023-10-11 14:13:54 -0300

    Criando tela de feed e função para ler os posts

--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,10 +1,19 @@
+import { lerPosts } from '../../firebase/firestore.js';
+ 
 export default () => {
     const container = document.createElement('section');
     const template = `
             <h1>Olá! Fique por dentro das pérolas que rolaram no Checkout!</h1>
+            <button id="mostrar">Mostrar</button>
             `;
 
     container.innerHTML = template;
+
+    const mostrarBtn = container.querySelector("#mostrar");
+    mostrarBtn.addEventListener('click', () => {
+      lerPosts();
+    });
+
 
     return container;
 };
